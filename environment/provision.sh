653de4223f7e8015dd6a54258915e713bbdd309e
Stephen Reid <steveyblam@hotmail.co.uk>
2018-01-03 11:50:40 +0000

    added auto yes to provisioner

--- environment/provision.sh
+++ environment/provision.sh
@@ -14,7 +14,7 @@ sudo apt-get install nginx -y
 sudo apt-get install git -y
 
 # install nodejs
-sudo apt-get install python-software-properties
+sudo apt-get install python-software-properties -y
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install nodejs -y
 
