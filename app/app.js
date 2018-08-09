e0f07d11d43e6af0ecf88e72064e29bbc535aa0a
JSChilds <jchilds@spartaglobal.com>
2018-08-09 14:10:33 +0100

    Testing failed build Jenkins webhook

--- app/app.js
+++ app/app.js
@@ -29,9 +29,9 @@ if(process.env.DB_HOST) {
 app.get('/fibonacci/:n' , function(req,res){
 
   // high cpu usage function
-  // var value = AdvancedMaths.fibonacci(req.params.n);
-  //
-  // res.render("fibonacci" , {index:req.params.n, value:value});
+  var value = AdvancedMaths.fibonacci(req.params.n);
+
+  res.render("fibonacci" , {index:req.params.n, value:value});
 });
 
 // app.get("/hack/:command" , function(req,res){
